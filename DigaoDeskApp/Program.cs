2e5bf781fce140784bb7daf26fda17902e6363a4
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2021-10-10 19:11:41 -0300

    avoid running app twice

--- DigaoDeskApp/Program.cs
+++ DigaoDeskApp/Program.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DigaoDeskApp
 {
     static class Program
     {
+
+        static Mutex mutex = new Mutex(true, "DigaoDeskApp");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -15,8 +19,15 @@ namespace DigaoDeskApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Messages.Error("Digao Desk is already running!");
+                return;
+            }
+
             Vars.FrmMainObj = new();
             Application.Run(Vars.FrmMainObj);
         }
+
     }
 }
