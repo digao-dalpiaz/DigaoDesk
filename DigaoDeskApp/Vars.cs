9551a2d789780445c67a5b0230723b76466f310c
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2022-09-12 23:17:39 -0300

    version 1.4.3

--- DigaoDeskApp/Vars.cs
+++ DigaoDeskApp/Vars.cs
@@ -5,7 +5,7 @@ namespace DigaoDeskApp
     class Vars
     {
 
-        public const string APP_VERSION = "1.4.2";
+        public const string APP_VERSION = "1.4.3";
         public const string APP_REGKEY = @"SOFTWARE\DigaoDesk";
 
         public const string GITHUB_LINK = "https://github.com/digao-dalpiaz/DigaoDesk";
