0622eaa8a2ab4503cdb63bbf4a86eda02eb85a65
Rodrigo Dalpiaz <dalpiaze@gmail.com>
2023-08-15 23:20:09 -0300

    oldconfig class

---
+++ DigaoDeskApp/Config/OldConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace DigaoDeskApp
+{
+    public class OldConfig
+    {
+        public string ReposDir;
+        public string ShellProgram;
+        public string DiffProgram;
+        public string DiffProgramArguments;
+        public string GitNewBranchPrefixList;
+        public string GitCustomCommands;
+        public bool GitAutoCRLF;
+        public bool GitAutoFetch;
+        public string GitCommitMessage;
+        public ConfigGit Git;
+
+        public class ConfigGit
+        {
+            public string Name;
+            public string Email;
+
+            public string CredUsername;
+            public string CredPassword;
+        }
+    }
+}
